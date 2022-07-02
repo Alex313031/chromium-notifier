ec65e9630ad81a49657ed4a48c4cdeb35c7316b6
Alexander David Frick <Alex313031@gmail.com>
2022-07-02 11:42:13 -0500

    Add files via upload

---
+++ src/js/popup.js
@@ -0,0 +1,428 @@
+import { Component, h, render } from './vendor/preact-10.0.1.js'
+import htm from './vendor/htm-2.2.1.js'
+import {
+  clearError,
+  getConfig,
+  getExtensionsInfo,
+  getUserAgentData,
+  matchExtension,
+  trackError
+} from './utils.js'
+
+window.onerror = e => {
+  trackError(e)
+  return false
+}
+
+window.onunhandledrejection = e => {
+  trackError(e)
+  return false
+}
+
+const html = htm.bind(h)
+
+/*
+ * Event handlers
+ */
+
+const changeBoolSetting = ({ target: { checked, name } }) => {
+  const newState = {
+    [name]: checked
+  }
+
+  if (name === 'extensionsTrack' && checked) {
+    getUserAgentData()
+      .then(({ uaFullVersion }) => getExtensionsInfo(uaFullVersion))
+      .then(extensionsInfo => {
+        newState.extensionsInfo = extensionsInfo
+      })
+      .finally(() => {
+        chrome.storage.local.set(newState)
+      })
+  } else {
+    chrome.storage.local.set(newState)
+  }
+}
+
+const changePlatform = e =>
+  chrome.storage.local.set({
+    arch: e.target.value,
+    tag: null
+  })
+
+const changeTag = e => chrome.storage.local.set({ tag: e.target.value })
+
+const removeExt = e => chrome.management.uninstall(e.target.id)
+
+/*
+ * Components
+ */
+
+const ChromiumInfo = ({ arch, current = {}, currentVersion, tag }) => html`
+  <details open="${current.version !== currentVersion}">
+    <summary>Chromium <code>v${currentVersion}</code></summary>
+    <ul>
+      <li>
+        <span>Current: </span>
+        <code class="${current.version !== currentVersion && 'badge'}"
+          >v${current.version}</code
+        >
+      </li>
+      <li>
+        <span>Revision: ${current.revision} </span>
+        (${new Date(current.timestamp * 1000).toLocaleString()})
+      </li>
+      ${current.links &&
+        html`
+          <li>
+            <span>Downloads: </span>
+            ${current.links.map(
+              ({ label, url }, i) => html`
+                <a href="${url}" target="_blank">${label}</a>
+                ${i + 1 < current.links.length && ', '}
+              `
+            )}
+          </li>
+        `}
+    </ul>
+    <div style="font-size: smaller; margin-top: 1em">
+      <span>Tracking </span>
+      <a href="https://chromium.woolyss.com/#${arch}-${tag}" target="_blank"
+        >${arch}-${tag}</a
+      >
+    </div>
+  </details>
+`
+
+const ExtensionsInfo = ({
+  currentVersion,
+  extensions = [],
+  extensionsInfo = [],
+  onDisableExtension
+}) => {
+  const supported = extensions
+    .filter(ext => extensionsInfo.find(matchExtension(ext)))
+    .sort((a, b) => a.name.localeCompare(b.name))
+
+  const unsupported = extensions
+    .filter(ext => !supported.find(({ id }) => id === ext.id))
+    .sort((a, b) => a.name.localeCompare(b.name))
+
+  return html`
+    <details
+      open="${!extensionsInfo.every(e =>
+        extensions.find(({ version }) => version === e.version)
+      )}"
+    >
+      <summary>${extensions.length} Extensions</summary>
+      <ul class="extensions">
+        ${supported.map(ext => {
+          const info = extensionsInfo.find(matchExtension(ext))
+          return html`
+            <li>
+              <div class="${ext.enabled ? '' : ' disabled'}">
+                <input
+                  checked="${ext.enabled}"
+                  id="${ext.id}"
+                  onChange="${onDisableExtension}"
+                  style="margin-right: 0.75em"
+                  title="${ext.enabled ? 'Disable' : 'Enable'}"
+                  type="checkbox"
+                />
+                ${ext.homepageUrl
+                  ? html`
+                      <a href="${ext.homepageUrl}" target="_blank"
+                        ><span>${ext.name} </span>
+                      </a>
+                    `
+                  : `${ext.name} `}
+                <code
+                  ><span>v${ext.version} </span> ${info.status !== 'noupdate' &&
+                    info.version !== ext.version &&
+                    html`
+                      <a
+                        class="badge"
+                        href="${info.codebase.includes(
+                          'clients2.googleusercontent.com'
+                        )
+                          ? `${info.updateUrl}?response=redirect&acceptformat=crx2,crx3&prodversion=${currentVersion}&x=id%3D${info.id}%26installsource%3Dondemand%26uc`
+                          : info.codebase}"
+                        target="_blank"
+                        >v${info.version}</a
+                      >
+                    `}</code
+                >
+              </div>
+              <div>
+                <button class="remove" id="${ext.id}" onClick="${removeExt}">
+                  🗑
+                </button>
+              </div>
+            </li>
+          `
+        })}
+      </ul>
+      ${unsupported.length > 0 &&
+        html`
+          <p style="margin-bottom: 0;">No update info available:</p>
+          <ul class="extensions">
+            ${unsupported.map(ext => {
+              const info = extensionsInfo.find(({ id }) => id === ext.id)
+              return html`
+                <li>
+                  <div class="${ext.enabled ? '' : ' disabled'}">
+                    <input
+                      checked="${ext.enabled}"
+                      id="${ext.id}"
+                      onChange="${onDisableExtension}"
+                      style="margin-right: 0.75em"
+                      title="${ext.enabled ? 'Disable' : 'Enable'}"
+                      type="checkbox"
+                    />
+                    ${ext.homepageUrl
+                      ? html`
+                          <a href="${ext.homepageUrl}" target="_blank"
+                            ><span>${ext.name} </span>
+                          </a>
+                        `
+                      : `${ext.name} `}
+                    <code>v${ext.version}</code>
+                  </div>
+                  <div>
+                    <button
+                      class="remove"
+                      id="${ext.id}"
+                      onClick="${removeExt}"
+                    >
+                      🗑
+                    </button>
+                  </div>
+                </li>
+              `
+            })}
+          </ul>
+        `}
+    </details>
+  `
+}
+
+const Header = ({ version }) => html`
+  <div>
+    <div>
+      <p style="color: #202124; margin: 0">
+        <strong>Chromium Update Notifications </strong>
+        <code>${version && `v${version}`}</code>
+      </p>
+      <span>based on </span>
+      <a href="https://chromium.woolyss.com/" target="_blank">Woolyss</a>
+    </div>
+    <div class="header-cell">
+      <a href="https://github.com/kkkrist/chromium-notifier" target="_blank">
+        <img src="../img/github.svg" style="height: 1rem; width: auto;" />
+      </a>
+    </div>
+  </div>
+`
+
+class Section extends Component {
+  state = { errorMsg: null }
+
+  componentDidCatch (error) {
+    this.setState({ errorMsg: error.message })
+  }
+
+  render ({ children }, { errorMsg }) {
+    return html`
+      <section>
+        ${errorMsg
+          ? html`
+              <small style="color: red">${errorMsg}</small>
+            `
+          : children}
+      </section>
+    `
+  }
+}
+
+const Settings = ({
+  arch,
+  errorTracking,
+  extensionsTrack,
+  tag,
+  versions
+}) => html`
+  <details open="${!arch || !tag}">
+    <summary>Settings</summary>
+    <div>
+      <label>
+        <p>Platform</p>
+        <select
+          disabled="${!Object.keys(versions).length}"
+          onChange="${changePlatform}"
+        >
+          <option disabled="${arch && versions[arch]}" value=""
+            >Choose platform…</option
+          >
+          ${Object.keys(versions).map(
+            archOpt => html`
+              <option selected="${archOpt === arch}" value="${archOpt}"
+                >${archOpt}</option
+              >
+            `
+          )}
+        </select>
+      </label>
+      <label>
+        <p>Tag</p>
+        <select disabled="${!arch || !versions[arch]}" onChange="${changeTag}">
+          <option disabled="${tag}" value="">Choose tag…</option>
+          ${arch &&
+            versions[arch] &&
+            versions[arch].map(
+              tagOpts => html`
+                <option selected="${tagOpts.tag === tag}" value="${tagOpts.tag}"
+                  >${tagOpts.tag}</option
+                >
+              `
+            )}
+        </select>
+      </label>
+      
+      <br />
+      
+      <p style="margin: 0;">
+        <label>
+          <input
+            checked="${errorTracking || errorTracking === undefined}"
+            name="errorTracking"
+            onChange="${changeBoolSetting}"
+            style="margin: 0 0.25rem 0 0"
+            type="checkbox"
+          />
+          Enable error tracking
+        </label>
+      </p>
+    </div>
+  </details>
+`
+
+/*
+ * Main app
+ */
+
+class App extends Component {
+  state = {
+    extensions: [],
+    extensionsInfo: [],
+    self: {},
+    versions: {}
+  }
+
+  onDisableExtension = ({ target: { checked, id } }) => {
+    chrome.management.setEnabled(id, checked)
+
+    const newState = [...this.state.extensions]
+    const i = newState.findIndex(e => e.id === id)
+    newState[i].enabled = checked
+    this.setState({ extensions: newState })
+  }
+
+  onStorageChanges = changes => {
+    this.setState(
+      Object.keys(changes).reduce(
+        (acc, key) => ({ ...acc, [key]: changes[key].newValue }),
+        {}
+      )
+    )
+  }
+
+  async componentDidMount () {
+    await clearError()
+    const config = await getConfig()
+    this.setState(config)
+    chrome.storage.onChanged.addListener(this.onStorageChanges)
+  }
+
+  componentWillUnmount () {
+    chrome.storage.onChanged.removeListener(this.onStorageChanges)
+  }
+
+  render (
+    props,
+    {
+      arch,
+      currentVersion,
+      error,
+      errorTracking,
+      extensions,
+      extensionsInfo,
+      extensionsTrack,
+      self,
+      tag,
+      timestamp,
+      versions
+    }
+  ) {
+    const current =
+      arch && versions[arch] && versions[arch].find(v => v.tag === tag)
+
+    return html`
+      <${Section}><${Header} version="${self && self.version}"/><//>
+
+      ${arch &&
+        tag &&
+        html`
+          <${Section}>
+            <${ChromiumInfo}
+              arch="${arch}"
+              current="${current}"
+              currentVersion="${currentVersion}"
+              tag="${tag}"
+            />
+          <//>
+        `}
+      ${extensionsTrack &&
+        html`
+          <${Section}>
+            <${ExtensionsInfo}
+              currentVersion="${currentVersion}"
+              extensions="${extensions}"
+              extensionsInfo="${extensionsInfo}"
+              onDisableExtension="${this.onDisableExtension}"
+            />
+          <//>
+        `}
+
+      <${Section}>
+        <${Settings}
+          arch="${arch}"
+          errorTracking="${errorTracking}"
+          extensionsTrack="${extensionsTrack}"
+          tag="${tag}"
+          versions="${versions}"
+        />
+      <//>
+
+      <${Section}>
+        <small>
+          ${timestamp
+            ? `Last update: ${new Date(timestamp).toLocaleString()}`
+            : `Waiting for data…`}
+        </small>
+        ${error &&
+          html`
+            <small style="color: red; margin-top: 0.5rem;">
+              Last error: ${error}
+            </small>
+          `}
+      <//>
+    `
+  }
+}
+
+render(
+  html`
+    <${App} />
+  `,
+  document.getElementById('app')
+)
---
+++ src/js/utils.js
@@ -0,0 +1,218 @@
+const parser = new DOMParser()
+
+const addIfNew = (arr = [], item) =>
+  item === undefined ? arr : [...new Set([...arr]).add(item)]
+
+export const clearError = () =>
+  new Promise(resolve =>
+    chrome.action.setBadgeText({ text: '' }, () =>
+      chrome.action.setBadgeBackgroundColor(
+        { color: [0, 150, 180, 255] },
+        () => chrome.storage.local.set({ error: null }, () => resolve())
+      )
+    )
+  )
+
+export const getSelf = () =>
+  new Promise(resolve => chrome.management.get(chrome.runtime.id, resolve))
+
+const fetchExtensionInfo = async (updateUrl, ids, prodversion) => {
+  const x = ids.map(id => `x=${encodeURIComponent(`id=${id}&uc`)}`).join('&')
+
+  const txt = await fetch(
+    `${updateUrl}?acceptformat=crx2,crx3&prodversion=${prodversion}&${x}`
+  ).then(req => req.text())
+
+  const xml = parser.parseFromString(txt, 'text/xml')
+
+  return Array.from(xml.querySelectorAll('app')).map(el => {
+    const updatecheck = el.querySelector('updatecheck')
+
+    const info = {
+      id: el.getAttribute('appid'),
+      prodversion,
+      timestamp: new Date().getTime(),
+      updateUrl
+    }
+
+    return updatecheck
+      ? {
+          ...info,
+          ...updatecheck.getAttributeNames().reduce(
+            (acc, attr) => ({
+              ...acc,
+              [attr]: updatecheck.getAttribute(attr)
+            }),
+            {}
+          )
+        }
+      : info
+  })
+}
+
+const fetchExtensionsInfo = async (extensions, prodversion) => {
+  const { useProxy } = await getConfig()
+  const self = await getSelf()
+
+  if (useProxy || useProxy === false) {
+    const res = await fetch(
+      `https://chrome-extension-service-kkkrist.vercel.app/api?pluginVersion=${self &&
+        self.version}`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          prodversion,
+          extensions
+        })
+      }
+    )
+
+    const text = await res.text()
+
+    try {
+      const json = JSON.parse(text)
+      return json
+    } catch (error) {
+      throw new Error(
+        `${error.message} (proxy API): ${
+          text.length > 60
+            ? text.slice(0, 30) + '…' + text.slice(text.length - 30)
+            : text
+        }`
+      )
+    }
+  } else {
+    const jobs = extensions.reduce((acc, { id, updateUrl }) => {
+      if (updateUrl) {
+        acc[updateUrl] = addIfNew(acc[updateUrl], id)
+      }
+      return acc
+    }, {})
+
+    const results = await Promise.allSettled(
+      Object.keys(jobs).map(
+        updateUrl =>
+          updateUrl &&
+          fetchExtensionInfo(updateUrl, jobs[updateUrl], prodversion)
+      )
+    )
+
+    return results
+      .filter(({ status }) => status === 'fulfilled')
+      .map(({ value }) => value)
+      .flat()
+  }
+}
+
+export const getUserAgentData = async () => {
+  if (navigator.userAgentData) {
+    const data = await navigator.userAgentData.getHighEntropyValues([
+      'platform',
+      'uaFullVersion'
+    ])
+    return data
+  }
+
+  const uaFullVersion = navigator.userAgent.match(
+    /Chrome\/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
+  )[1]
+
+  const platform = navigator.userAgent.includes('Macintosh')
+    ? 'macOS'
+    : navigator.userAgent.includes('Win64')
+    ? 'Windows'
+    : navigator.userAgent.includes('Windows')
+    ? 'Windows'
+    : undefined
+
+  return { platform, uaFullVersion }
+}
+
+export const getConfig = () =>
+  new Promise(resolve =>
+    getUserAgentData().then(({ platform, uaFullVersion }) => {
+      chrome.management.getAll(extensions =>
+        chrome.storage.local.get(store => {
+          if (!store.arch) {
+            store.arch = platform.includes('Macintosh')
+              ? 'mac'
+              : platform.includes('Win64')
+              ? 'win64'
+              : undefined
+          }
+          getSelf().then(self =>
+            resolve({
+              ...store,
+              currentVersion: uaFullVersion,
+              extensions,
+              self
+            })
+          )
+        })
+      )
+    })
+  )
+
+export const getExtensionsInfo = async currentVersion => {
+  await clearError()
+  const extensions = await new Promise(resolve =>
+    chrome.management.getAll(exts =>
+      resolve(
+        exts.map(ext => ({
+          id: ext.id,
+          updateUrl: ext.updateUrl
+        }))
+      )
+    )
+  )
+
+  return await fetchExtensionsInfo(extensions, currentVersion)
+}
+
+export const matchExtension = ext => ({ id, updateUrl, version }) => {
+  if (!version) {
+    return false
+  }
+
+  if (id === ext.id) {
+    return true
+  }
+
+  if (
+    updateUrl !== 'https://clients2.google.com/service/update2/crx' &&
+    updateUrl === ext.updateUrl
+  ) {
+    return true
+  }
+}
+
+export const trackError = async e => {
+  console.error(e.reason || e.error || e)
+  const message = e.reason?.stack || e.stack || e.message || 'Unknown'
+
+  try {
+    const self = await getSelf()
+    const { errorTracking } = await getConfig()
+
+    chrome.storage.local.set({
+      error: message
+    })
+
+    if (errorTracking || errorTracking === undefined) {
+      fetch(
+        'https://chrome-extension-service-kkkrist.vercel.app/api/errorlogs',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            error: message,
+            pluginVersion: self && self.version
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        }
+      )
+    }
+  } catch (error) {
+    console.error(`Error while error tracking, d'oh!`, error)
+  }
+}
